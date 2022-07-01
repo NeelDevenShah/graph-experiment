851811353b26390576af51ec8553ee904a75055e
Neel Shah <neeldevenshah@gmail.com>
2022-07-02 03:20:52 +0530

    Comparision of more than two datas in line chart and bar chart

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import { useState } from "react";
-import "./App.css";
-// import BarChart from "./components/BarChart";
-// import LineChart from "./components/LineChart";
-import PieChart from "./components/PieChart";
-import BarChart from "./components/BarChart";
-import LineChart from "./components/LineChart";
-import { UserData } from "./Data";
+import React, { useState } from 'react'
+import LineChart from './components/LineChart'
+import BarChart from './components/BarChart'
+import PieChart from './components/PieChart'
+import {UserData} from './Data'
+import {UserData1} from './Data'
 
 function App() {
   const [userData, setUserData] = useState({
     labels: UserData.map((data) => data.year),
     datasets: [
       {
-        label: "Users Gained",
+        label: "Users Gained IPL",
         data: UserData.map((data) => data.userGain),
         backgroundColor: [
           "rgba(75,192,192,1)",
@@ -24,24 +22,37 @@ function App() {
         borderColor: "black",
         borderWidth: 2,
       },
+
+      {
+        label: "Users Gained PIL",
+        data: UserData1.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+
     ],
   });
 
-  // IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
-
   return (
-    <div className="App">
-      <div style={{ width: 700 }}>
-        <BarChart chartData={userData} />
+    <div>
+      <div>
+        <div style={{width: 700}}>
+        <LineChart chartData={userData}/>
+        </div>
+
+        <div style={{width: 700}}>
+        <BarChart chartData={userData}/>
+        </div>
       </div>
-      <div style={{ width: 700 }}>
-        <LineChart chartData={userData} />
-      </div>
-      <div style={{ width: 200 }}>
-        <PieChart chartData={userData} />
     </div>
-    </div>
-  );
+  )
 }
 
-export default App;
+export default App
